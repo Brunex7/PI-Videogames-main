288102198d6134c0c6c49309589763d2b219b263
Brunex7 <brunoromero200@gmail.com>
2023-07-19 12:25:27 -0300

    Cambios Detail

--- PI-Videogames-main/client/src/views/Detail/Detail.jsx
+++ PI-Videogames-main/client/src/views/Detail/Detail.jsx
@@ -1,50 +1,46 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetailGames } from "../../redux/action";
-import style from './Detail.module.css'
+import style from './Detail.module.css';
 
-const Detail = (props) =>{
-
+const Detail = (props) => {
     const dispatch = useDispatch();
-    
 
-    useEffect(() =>{
+    useEffect(() => {
         dispatch(getDetailGames(props.match.params.id));
-    },[dispatch, props.match.params.id]);
-    
-    const game = useSelector((state) => state.detailGame)
-    
+    }, [dispatch, props.match.params.id]);
+
+    const game = useSelector((state) => state.detailGame);
+
     return (
-        <div className={style.conte}>{}
+        <div className={style.conte}>
             <div className={style.title}>
                 <h1>{game.name}</h1>
             </div>
             <div className={style.imag}>
                 <img src={game.background_image} alt={game.name} />
+                <div className={style.desc} dangerouslySetInnerHTML={{ __html: game.description }}></div>
+                <div className={style.gpr}>
+                    <div>
+                        <h2>Genres</h2>
+                        <ul className={style.gen}>{game.genres && game.genres.map(g => (<li key={g.id}>{g.name}</li>))}</ul>
+                    </div>
+                    <div>
+                        <h2>Platforms</h2>
+                        <ul className={style.plat}>{game.platforms?.map(el => el.platform.name).join(', ')}</ul>
+                    </div>
+                    <div>
+                        <h2>Rating</h2>
+                        <p>{game.rating}</p>
+                    </div>
+                    <div>
+                        <h2>Released</h2>
+                        <p>{game.released}</p>
+                    </div>
+                </div>
             </div>
-            <div className={style.desc}>
-                <p dangerouslySetInnerHTML={ {__html:game.description}}></p>
-            </div>
-            <div className={style.gpr}>
-            <div className={style.gen}>
-                <h2>Genres</h2>
-                <ul>{game.genres && game.genres?.map(g => (<p key ={g.id} >{g.name}</p>))}</ul>
-            </div>
-            <div className={style.plat}>
-                <h2>Platforms</h2>
-                <ul>{game.platforms?.map(el => el.platform.name).toString()}</ul>
-            </div>
-            <div> 
-                <h2>Rating</h2>
-                <p>{game.rating}</p>
-            </div>
-            <div> 
-                <h2>Relased</h2>
-                <p>{game.released}</p>
-            </div>
-            </div>
-
         </div>
-    )
-}
+    );
+};
+
 export default Detail;
